0dd674bd7986d3cedaeca1932a06a1e68b67c5b6
rlo-lo <148983524+rlo-lo@users.noreply.github.com>
2025-01-06 10:11:25 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250106/거품 정렬 구현/implement-bubble-sort.cpp
+++ 250106/거품 정렬 구현/implement-bubble-sort.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
-#include<vector>
+#include <algorithm>
+
+#define MAX_N 100
+
 using namespace std;
 
+// 변수 선언
+int n;
+int arr[MAX_N];
 
+void BubbleSort() {
+    bool sorted = true;
+
+    do {
+        sorted = true;
+        for(int i = 0; i < n - 1; i++)
+            if(arr[i] > arr[i + 1]) {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                sorted = false;
+            }//바로 뒤가 sorting 되었다면 그 뒤는 모두 sorting 되어 있음
+    } while(!sorted);
+}
 
 int main() {
-   int n; 
-   cin >> n; 
-   int k ; 
-   vector<int> v(n); 
-   for(int i = 0;  i< n; i++){
-        cin >> k ;
-        v[i] = k; 
-   }
+    // 입력
+    cin >> n;
 
-   for(int i  = 0; i<v.length()-1; i++){
-        for(int j= 0; i<v.length(); j++){
-            if(v[j]> v[j+1]){
-                int temp = v[j]; 
-                v[j] = v[j+1]; 
-                v[j+1] = temp; 
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    BubbleSort();
 
-            }
-        }
-   }
-
-   for(int i = 0;  i< n; i++){
-        cout << v[j]<< " "; 
-   }
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    return 0;
 }
